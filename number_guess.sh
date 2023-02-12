9916a3a061c65f7af9200ae7705b09287f873c00
alancj731 <alanchenjian@gmail.com>
2023-02-12 05:00:39 +0000

    feat: add start message

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+# ask user to enter his name
 echo Enter your username:
+# read user input to USERNAME
 read USERNAME
+
+# check if it is a new user
+RECORD=$($PSQL "SELECT * FROM records WHERE user_name='$USERNAME'")
+
+if [[ -z $RECORD ]]
+then
+	echo Welcom $USERNAME
+else
+	echo User info: $RECORD
+fi
+
