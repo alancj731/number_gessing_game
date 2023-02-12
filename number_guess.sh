6c0652747c79e81684ca5b6415350c40fae460da
alancj731 <alanchenjian@gmail.com>
2023-02-12 12:37:50 +0000

    feat: query customer's info from database

--- number_guess.sh
+++ number_guess.sh
@@ -8,9 +8,16 @@ read USERNAME
 # check if it is a new user
 RECORD=$($PSQL "SELECT * FROM records WHERE user_name='$USERNAME'")
 
-if [[ -z $RECORD ]]
+if [[ ! -z $RECORD ]]
 then
-	echo Welcom $USERNAME
+	# get user's information from database
+	USERINFO=$($PSQL "SELECT games_played, best_game FROM records WHERE user_name='$USERNAME';")
+	# relpace | with space in query result
+	USERINFO=$(echo $USERINFO | sed 's/|/ /g')
+	# read games_played, best_game data from query result
+	read GAMES_PLAYED BEST_GAME <<< $USERINFO
+	# print the welcome message
+	echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses." 
 else
 	echo User info: $RECORD
 fi
